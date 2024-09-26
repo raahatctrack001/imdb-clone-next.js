b8a839f4c65070127cc3a60a7f06204a8b61bb40
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-09-26 22:28:31 +0530

    add error handler file

---
+++ src/app/error.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { useEffect } from 'react';
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+  return (
+    <div className='text-center min-h-screen w-full flex flex-col justify-center items-center gap-5'>
+      <h1>Something went wrong. Please try again later.</h1>
+      <button className='px-5 py-2 bg-red-500 text-white font-semibold rounded-lg' onClick={() => reset()}>
+        Try Again
+      </button>
+    </div>
+  );
+}
