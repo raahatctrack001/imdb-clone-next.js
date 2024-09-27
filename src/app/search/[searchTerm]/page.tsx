1c7d967d8bdce88ccd18c99e6e178defcc8e7dbe
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-09-27 10:29:55 +0530

    solve error for production

--- src/app/search/[searchTerm]/page.tsx
+++ src/app/search/[searchTerm]/page.tsx
@@ -2,6 +2,7 @@ import FetchedResult from "@/components/movieCards/fetchedResult";
 
 
 export default async function SearchPage({ params }: {params: {searchTerm: string}}) {
+
   const { searchTerm } = params;
   const res = await fetch(
     `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`
--- src/components/Search/SearchBox.tsx
+++ src/components/Search/SearchBox.tsx
@@ -3,28 +3,33 @@
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
-export default function SearchBar(){
-    const [ searchTerm, setSearchTerm ] = useState<string>("")
+export default function SearchBar() {
+    const [searchTerm, setSearchTerm] = useState("")
     const router = useRouter();
-    useEffect(()=>{
-        if(searchTerm.trim()===''){
+
+    useEffect(() => {
+        if (searchTerm.trim() === '') {
             router.push("/")
         }
-        if(searchTerm.trim()){
+        if (searchTerm.trim()) {
             const timeout = setTimeout(() => {
-                if(searchTerm?.trim())
+                if (searchTerm?.trim())
                     router.push(`/search/${searchTerm}`);
             }, 2000);
-            return ()=>clearTimeout(timeout)
+            return () => clearTimeout(timeout)
         }
-         
+
     }, [searchTerm, router])
-    return <div className="w-full grid place-items-center">
-        <input 
-            value={searchTerm}
-            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setSearchTerm(e.target.value)}
-            placeholder="search movies" 
-            className="w-full max-w-5xl py-2 pl-5 text-lg rounded-lg mt-5 bg-gray-700 dark:bg-gray-950 "
-        />        
-    </div>
+
+    return (
+        <div className="w-full grid place-items-center">
+            <input
+                value={searchTerm}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)}
+                placeholder="search movies"
+                className="w-full max-w-5xl py-2 pl-5 text-lg rounded-lg mt-5 bg-gray-700 dark:bg-gray-950"
+                // Avoid passing any unnecessary props or attributes
+            />
+        </div>
+    )
 }
